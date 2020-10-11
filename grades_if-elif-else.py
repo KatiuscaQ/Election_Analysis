353ebd13a5ee46d681714546686055570a93a349
Katiusca <quijadakatiusca@gmail.com>
2020-10-11 00:45:51 -0400

    Adding PyPoll.py file.

---
+++ grades_if-elif-else.py
@@ -0,0 +1,15 @@
+#What is the score
+score = int(input("What is your test score?"))
+
+#Getting the grade
+if score >= 90:
+    print("Your grade is A.")
+elif score >= 80:
+    print("Your grade is B.")
+elif score >= 70:
+    print("Your grade is C.")
+elif score >= 60:
+    print("Your grade is D.")
+else:
+    print("Your grade is F.")
+    
